19fe7c681f0cb674912e6483591f11fd95520651
Afonso Muralha <afonsomuralha@gmail.com>
2018-04-25 18:38:49 +0100

    added logo to splashscreen

--- Firmware/Base Firmware/oled.cpp
+++ Firmware/Base Firmware/oled.cpp
@@ -1,23 +1,7 @@
 
 #include "oled.h"
+#include "splash.h"
 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
 
 
 
@@ -34,8 +18,8 @@ void setupOLED()   {
   // Show image buffer on the display hardware.
   // Since the buffer is intialized with an Adafruit splashscreen
   // internally, this will display the splashscreen.
-  display.display();
-  delay(2000);
+  //display.display();
+  //delay(2000);
 
   // Clear the buffer.
   display.clearDisplay();
@@ -53,7 +37,7 @@ void setupOLED()   {
 
 void drawSplash(){
 
-  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
+  display.drawBitmap(0, 0,  Splash, 128, 32, 1);
   display.display();
   delay(1);
 
