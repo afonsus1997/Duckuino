9f615e652a3572949f0dc434f785f62cc4caacfd
Afonso Muralha <afonso.muralha@tecnico.ulisboa.pt>
2019-03-12 22:09:43 +0000

    Code still a mess, added special splash screen for pixels camp v3

--- Code/Firmware/Release/core/IPAddress.cpp.d
+++ Code/Firmware/Release/core/IPAddress.cpp.d
@@ -12,5 +12,5 @@ C:\Users\Afonso\ Muralha\AppData\Local\Temp\VMBuilds\Firmware\SparkFun_promicro_
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBDesc.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBCore.h \
- C:\Users\Afonso\ Muralha\AppData\Local\arduino15\packages\SparkFun\hardware\avr\1.1.12\variants\promicro/pins_arduino.h \
+ C:\Users\Afonso\ Muralha\AppData\Local\arduino15\packages\SparkFun\hardware\avr\1.1.11\variants\promicro/pins_arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/IPAddress.h
